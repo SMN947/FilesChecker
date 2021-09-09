b3fd90b0a18c64274604adb8a288e303fb205fe9
SMN947 <betum98@gmail.com>
2021-09-09 13:49:47 -0500

    added DB control

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // Modules to control application life and create native browser window
+const {JsonDB} = require('node-json-db');
+const { Config } = require('node-json-db/dist/lib/JsonDBConfig');
 const {app, BrowserWindow} = require('electron')
+const os = require('os');
 const path = require('path')
+
+var db = new JsonDB(new Config("DB_"+os.userInfo().username, true, false, '/'));
 
 function createWindow () {
   // Create the browser window.
@@ -26,18 +31,28 @@ app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
+//DB Controller
+
+function addFolder(fPath, tbName) {
+  var json = {};
+  json[tbName] = {
+      fPath: fPath
+  }
+  console.log(json)
+  db.push("/folders",json, false);
+}
+function removeFolder(tbName) {
+  db.delete("/folders/"+tbName);
+}
